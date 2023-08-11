bf531cc1123c4fcddcd94e264524dbe2abb8dee3
noname52rus <matvik52rus@gmail.com>
2023-08-11 10:30:47 +0300

    11_08_2023

--- app/main.py
+++ app/main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from .library.helpers import *
+from app.routers import unsplash
+
 
 app = FastAPI()
+app.include_router(unsplash.router)
+
 
 templates = Jinja2Templates(directory='templates')
 app.mount('/static', StaticFiles(directory='static'), name="static")
@@ -11,15 +16,11 @@ app.mount('/static', StaticFiles(directory='static'), name="static")
 
 @app.get('/', response_class=HTMLResponse)
 async def home(request: Request):
-    data = {
-        'page' : 'Home Page'
-    }
+    data = openfile('home.md')
     return templates.TemplateResponse('page.html', {'request': request, 'data': data})
 
 
 @app.get('/page/{page_num}', response_class=HTMLResponse)
 async def page(request: Request, page_num: str):
-    data = {
-        'page' : page_num
-    }
+    data = openfile(page_num + '.md')
     return templates.TemplateResponse('page.html', {'request': request, 'data': data})
